624dad07d681ead03c15163d2678961869f53b0c
Saifurrahmanemon <saifemon789@gmail.com>
2023-01-14 15:30:24 +0600

    feat: configure abs path and add theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+import { Inter } from '@next/font/google';
+import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
-import '../styles/globals.css';
+import 'styles/globals.css';
+
+const interVariable = Inter();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <main className={interVariable.className}>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ function index() {
       <h1 className="text-2xl text-center text-amber-800 m-3 p-1 font-semibold">
         Hello world
       </h1>
+      <h2>hello world</h2>
     </div>
   );
 }
