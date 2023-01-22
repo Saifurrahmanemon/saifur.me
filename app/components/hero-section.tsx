d2fb8a73ff3e43760b87484d97a3eb0855b136d7
Saifurrahmanemon <saifemon789@gmail.com>
2023-01-22 22:28:25 +0600

    feat: add hero section

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -1,8 +1,27 @@
 function HeroSection() {
   return (
-    <div>
-      <h1>Hi This is Saifur Rahman Emon</h1>
-    </div>
+    <section className="mt-10">
+      <div className="flex justify-evenly items-start flex-col-reverse sm:flex-row">
+        <div className="flex flex-col pr-8">
+          <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1">
+            Saifur Rahman Emon
+          </h1>
+          <h2 className="text-gray-700 dark:text-gray-200 mb-4">
+            Software Developer at{' '}
+            <span className="font-semibold">Earnest Fintech LTD.</span>
+          </h2>
+          <p className="text-secondary mb-16 max-w-md">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit.
+          </p>
+        </div>
+        <div className="">
+          <div className=" animate-pulse rounded-full bg-slate-700 h-32 w-32"></div>
+        </div>
+      </div>
+    </section>
   );
 }
 
