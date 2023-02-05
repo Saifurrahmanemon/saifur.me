62fc339fd27f8ee138415de28be4953ef89905e8
Saifurrahmanemon <saifemon789@gmail.com>
2023-02-05 16:59:38 +0600

    feat: mobile menu

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -1,6 +1,6 @@
 function HeroSection() {
   return (
-    <section className="mt-10">
+    <section className="pt-20">
       <div className="flex justify-evenly items-start flex-col-reverse sm:flex-row">
         <div className="flex flex-col pr-8">
           <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1">
@@ -14,7 +14,153 @@ function HeroSection() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
             accusamus tempora dolores aperiam iste? Vero, eos dolores error
             magnam minima, laborum tempore odit facilis porro quaerat
-            voluptatibus molestiae, vitae sit.
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Incidunt accusamus tempora dolores
+            aperiam iste? Vero, eos dolores error magnam minima, laborum tempore
+            odit facilis porro quaerat voluptatibus molestiae, vitae sit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Enim aut dolores
+            debitis ad ipsa, in mollitia quas officiis modi exercitationem ut
+            neque impedit deserunt animi. Ex illo reiciendis omnis sit? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            accusamus tempora dolores aperiam iste? Vero, eos dolores error
+            magnam minima, laborum tempore odit facilis porro quaerat
+            voluptatibus molestiae, vitae sit. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Enim aut dolores debitis ad ipsa, in
+            mollitia quas officiis modi exercitationem ut neque impedit deserunt
+            animi. Ex illo reiciendis omnis sit?
           </p>
         </div>
         <div className="">
