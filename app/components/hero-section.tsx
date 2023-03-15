f77de9f1608055db48718cd99e3a4449b6da0f1f
Saifurrahmanemon <saifemon789@gmail.com>
2023-03-15 12:33:27 +0600

    feat: social links

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import { company, jobTitle, name } from '~/lib/user_details';
+import { GithubIcon, LinkedInIcon } from '~/ui/icons';
+
+const githubLink = 'https://github.com/Saifurrahmanemon';
+const linkedInLink = 'https://linkedin.com/in/saifurrahmanemon';
 
 function HeroSection() {
   return (
@@ -11,9 +15,17 @@ function HeroSection() {
         </div>
         <div className="flex flex-col mt-4 sm:mt-2">
           <h1 className="font-mono text-2xl sm:text-3xl">{name}</h1>
-          <h2 className="mb-4 text-gray-700 dark:text-gray-200">
+          <h2 className="mb-0 text-gray-700 dark:text-gray-200">
             {jobTitle} at <span className="font-semibold">{company}.</span>
           </h2>
+          <div className="flex items-center gap-1">
+            <a rel="noopener noreferrer" target="_blank" href={githubLink}>
+              <GithubIcon />
+            </a>
+            <a rel="noopener noreferrer" target="_blank" href={linkedInLink}>
+              <LinkedInIcon />
+            </a>
+          </div>
         </div>
       </div>
     </section>
