cc9ff6d095cb14d5f62a71ffb6b9d63a95b38e60
Saifurrahmanemon <saifemon789@gmail.com>
2023-10-04 23:05:37 +0600

    feat: about cotent

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,14 @@
+import { aboutMe } from '~/lib/user_details';
+import { navMaxWidth } from '~/ui/navbar';
+
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <main className={navMaxWidth}>
+      <section className="pt-20 mx-4">
+        <div className="flex items-center gap-1 my-10 sm:gap-4">{aboutMe}</div>
+      </section>
+    </main>
+  );
 }
 
 export default AboutPage;
--- lib/user_details.ts
+++ lib/user_details.ts
@@ -1,5 +1,11 @@
 /* eslint-disable quotes */
 export const name = 'Saifur Rahman Emon';
 export const jobTitle = 'Software Developer';
-export const company = 'Earnest Fintech LTD';
-export const aboutMe = '';
+export const company = 'Earnest Data Analytics';
+export const aboutMe = `
+Hello! I'm Saifur Rahman Emon, a dedicated software developer with a fervent passion for crafting exceptional digital products. When I'm not immersed in lines of code, you'll often find me lost in the world of literature, flipping through the pages of captivating books, or embracing the exhilarating freedom of the open road as I ride my bike.
+
+My journey in the world of software development has been nothing short of exhilarating. I thrive on the challenge of transforming innovative ideas into functional and user-friendly solutions that make a real difference. Whether it's crafting elegant algorithms or designing intuitive user interfaces, I relish every moment of the creative process.
+
+My coding adventures have not only honed my technical skills but also instilled in me a deep appreciation for collaboration and teamwork. I firmly believe that the best results are achieved when diverse talents come together to tackle complex problems. My ability to communicate and work harmoniously within a team is something I take immense pride in.
+`;
