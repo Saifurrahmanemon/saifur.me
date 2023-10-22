0f0e0cd78c14629b93fae45a8aefbd665e381771
Saifurrahmanemon <saifemon789@gmail.com>
2023-10-22 19:28:26 +0600

    fix: layout change

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,10 @@
 import { aboutMe } from '~/lib/user_details';
-import { navMaxWidth } from '~/ui/navbar';
 
 function AboutPage() {
   return (
-    <main className={navMaxWidth}>
-      <section className="pt-20 mx-4">
-        <div className="flex items-center gap-1 my-10 sm:gap-4">{aboutMe}</div>
-      </section>
-    </main>
+    <section className="pt-20 mx-4">
+      <div className="flex items-center gap-1 my-10 sm:gap-4">{aboutMe}</div>
+    </section>
   );
 }
 
