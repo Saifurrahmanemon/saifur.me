f29137caa921eb4030cbe46f294eff5d880c4b70
Saifurrahmanemon <saifemon789@gmail.com>
2023-10-29 18:45:49 +0600

    feat: blog page design and components

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,48 @@
+import Divider from '~/ui/divider';
+
+interface CardProps {
+  title: string;
+  date: string;
+  views?: string;
+}
+
+const Card = (props: CardProps) => {
+  const { title, date, views } = props;
+  return (
+    <div className="flex items-stretch justify-between w-full gap-2 px-3 py-2 transition-all rounded-lg cursor-pointer card-hover ">
+      <div className="relative flex flex-col ">
+        <span className="font-bold text-secondary">{title}</span>
+        {views && (
+          <span className="text-xs text-gray-600 dark:text-gray-400">
+            {views} Views
+          </span>
+        )}
+      </div>
+      <Divider />
+      <span className="text-secondary">{date}</span>
+    </div>
+  );
+};
+
 function BlogPage() {
+  const date = new Date().toLocaleDateString().toString();
+
   return (
     <>
-      <section className="pt-20 mx-4">blog</section>
+      <main className="pt-20 mx-4">
+        <section className="flex flex-col gap-4 pt-20 mx-auto">
+          {Array(10)
+            .fill(0)
+            .map((item) => (
+              <Card
+                key={item}
+                title="Lorem ipsum dolor sit amet"
+                views="43,432,423"
+                date={date}
+              />
+            ))}
+        </section>
+      </main>
     </>
   );
 }
