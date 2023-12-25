6ee578d8edbb3b1cb1cafb3ae8adef1064438949
Saifurrahmanemon <saifemon789@gmail.com>
2023-12-25 17:05:28 +0600

    feat: not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+function NotFoundPage() {
+  return (
+    <section className="mx-4">
+      <h1 className="my-2 text-md md:text-xl text-primary">
+        404 | This page could not be found.
+      </h1>
+      <p className="text-secondary">
+        Whoops! Looks like you took a detour into the unknown, much like a
+        spontaneous bike ride down unexplored paths. Embrace the thrill of this
+        digital trail. Just as every turn on a bike ride brings new sights,
+        every click might uncover something unexpected. Enjoy the ride!
+      </p>
+    </section>
+  );
+}
+
+export default NotFoundPage;
