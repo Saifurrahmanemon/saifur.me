12b837123edbe72b20913168a47584b6d54b3b5a
Saifurrahmanemon <saifemon789@gmail.com>
2024-01-13 18:31:54 +0600

    feat: sitemap && robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,13 @@
 import { domain } from '~/lib/user_details';
 
 export default function robots() {
-    return {
-        rules: [
-            {
-                userAgent: '*',
-            },
-        ],
-        sitemap: `https://${domain}/sitemap.xml`,
-        host: `https://${domain}`,
-    };
+  return {
+    rules: [
+      {
+        userAgent: '*'
+      }
+    ],
+    sitemap: `https://${domain}/sitemap.xml`,
+    host: `https://${domain}`
+  };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,17 +3,17 @@ import { domain } from '~/lib/user_details';
 import { getWritings } from '~/lib/writings';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const writing = getWritings().map((writing) => ({
-        url: `https://${domain}/writing/${writing.slug}`,
-        lastModified: writing.metadata.publishedAt,
-    }));
+  const writing = getWritings().map((writing) => ({
+    url: `https://${domain}/writing/${writing.slug}`,
+    lastModified: writing.metadata.publishedAt
+  }));
 
-    const links = ['', '/writing']
+  const links = ['', '/writing'];
 
-    let routes = links.map((route) => ({
-        url: `https://${domain}${route}`,
-        lastModified: new Date().toISOString().split('T')[0],
-    }));
+  let routes = links.map((route) => ({
+    url: `https://${domain}${route}`,
+    lastModified: new Date().toISOString().split('T')[0]
+  }));
 
-    return [...routes, ...writing];
+  return [...routes, ...writing];
 }
